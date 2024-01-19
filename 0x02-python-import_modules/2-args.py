f531cb5b58970e9f055bc4d5f7d72ad65d57fe46
Janejosh <bukunmi9758@gmail.com>
2024-01-19 03:13:08 -0800

    2-args.py

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from sys import argv
+    l = len(argv) - 1
+    if l < 1:
+        print("{} arguments.".format(l))
+    elif l == 1:
+        print("{} argument:".format(l))
+    else:
+        print("{} arguments:".format(l))
+        for a in range(l):
+            print("{}: {:s}".format(a + 1, argv[a + 1]))
